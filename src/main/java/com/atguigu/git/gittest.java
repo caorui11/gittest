dc658780764236316ddfaa1e9d08b742eb4dbf03
caorui <caorui@163.com>
2022-10-14 18:07:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gittest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-10-14 18:02
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
